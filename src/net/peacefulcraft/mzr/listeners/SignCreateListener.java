b9d40af6a111a79895ef74f003426360af46adc3
Parsonswy <parsonswy@gmail.com>
2021-05-23 17:19:20 -0400

    Fix sign create fetch lines

--- src/net/peacefulcraft/mzr/listeners/SignCreateListener.java
+++ src/net/peacefulcraft/mzr/listeners/SignCreateListener.java
@@ -1,9 +1,8 @@
 package net.peacefulcraft.mzr.listeners;
 
-import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.SignChangeEvent;
 
 import net.peacefulcraft.mzr.Mzr;
 import net.peacefulcraft.mzr.objective.Objective;
@@ -14,34 +13,31 @@ import net.peacefulcraft.mzr.objective.Objective;
 public class SignCreateListener implements Listener {
 	
 	@EventHandler
-	public void onSignPlace(BlockPlaceEvent ev) {
-		if (ev.getBlock() instanceof Sign) {
-			Sign s = (Sign) ev.getBlock();
-			if (s.getLines().length == 0) { return; }
+	public void onSignPlace(SignChangeEvent ev) {
+		if (ev.getLines().length == 0) { return; }
 
-			if (s.getLine(0).contains("[MZR]")) {
-				Objective objective = null;
-				try {
-					objective = Mzr._this().getObjectiveManager().getObjective(s.getLine(1));
-				} catch (IndexOutOfBoundsException ex) {}
-				if (objective == null) {
-					ev.getPlayer().sendMessage(Mzr.messagingPrefix + "Sign appears to be an MZR sign, but objective " + s.getLine(0) + " is not recognized.");
-					return;
-				}
+		if (ev.getLine(0).contains("[MZR]")) {
+			Objective objective = null;
+			try {
+				objective = Mzr._this().getObjectiveManager().getObjective(ev.getLine(1));
+			} catch (IndexOutOfBoundsException ex) {}
+			if (objective == null) {
+				ev.getPlayer().sendMessage(Mzr.messagingPrefix + "Sign appears to be an MZR sign, but objective " + ev.getLine(0) + " is not recognized.");
+				return;
+			}
 
-				String teleportTarget = "-1";
+			String teleportTarget = "-1";
+			try {
+				teleportTarget = ev.getLine(2);
+			} catch (IndexOutOfBoundsException ex) {}
+			if (teleportTarget.equalsIgnoreCase("resume")) {
+				ev.getPlayer().sendMessage(Mzr.messagingPrefix + "Sign syntax is valid.");
+			} else {
 				try {
-					teleportTarget = s.getLine(2);
-				} catch (IndexOutOfBoundsException ex) {}
-				if (teleportTarget.equalsIgnoreCase("resume")) {
+					objective.getCheckpoint(Integer.valueOf(teleportTarget));
 					ev.getPlayer().sendMessage(Mzr.messagingPrefix + "Sign syntax is valid.");
-				} else {
-					try {
-						objective.getCheckpoint(Integer.valueOf(teleportTarget));
-						ev.getPlayer().sendMessage(Mzr.messagingPrefix + "Sign syntax is valid.");
-					} catch (IndexOutOfBoundsException ex) {
-						ev.getPlayer().sendMessage(Mzr.messagingPrefix + "Sign appears to be an MZR sign for objective " + objective.getName() + ", but checkpoint " + s.getLine(3) + " is not known.");
-					}
+				} catch (IndexOutOfBoundsException ex) {
+					ev.getPlayer().sendMessage(Mzr.messagingPrefix + "Sign appears to be an MZR sign for objective " + objective.getName() + ", but checkpoint " + ev.getLine(2) + " is not known.");
 				}
 			}
 		}
